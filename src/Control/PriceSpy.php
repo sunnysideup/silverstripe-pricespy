39387dc523b4c0ab04576af1686aff17632953f6
SDGM <sion98madden@gmail.com>
2026-07-06 15:17:42 +1200

    Merge upgrades/automatedbranch/5to6 into main

--- src/Control/PriceSpy.php
+++ src/Control/PriceSpy.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sunnysideup\Pricespy\Control;
 
 use Sunnysideup\EcommerceGoogleShoppingFeed\Controllers\GoogleShoppingFeedController;
--- src/Providers/PriceSpyDataProvider.php
+++ src/Providers/PriceSpyDataProvider.php
@@ -1,17 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sunnysideup\Pricespy\Providers;
 
-use SilverStripe\Control\Controller;
-use SilverStripe\Control\Director;
-use SilverStripe\Core\Injector\Injector;
-use SilverStripe\ORM\DB;
-use SilverStripe\Versioned\Versioned;
-use Sunnysideup\Ecommerce\Api\Converters\CsvFunctionality;
-use Sunnysideup\Ecommerce\Api\ProductCollection;
-use Sunnysideup\Ecommerce\Model\Config\EcommerceDBConfig;
-use Sunnysideup\Ecommerce\Pages\Product;
 use Sunnysideup\EcommerceGoogleShoppingFeed\Api\ProductCollectionForGoogleShoppingFeed;
-use Sunnysideup\Pricespy\Interfaces\PriceSpyDataProviderInterface;
 
 class PriceSpyDataProvider extends ProductCollectionForGoogleShoppingFeed {}
